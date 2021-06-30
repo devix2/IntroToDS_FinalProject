312dbabc2f1aee8fdc6177e5fda9baeb32d2795a
devix2 <devixsc2@gmail.com>
2021-06-30 19:01:21 +0200

    Chiudo giornata
    Si deve finire il giro di importazione dei dati, poi si devono allacciare i databases secondo necessità (meglio farlo per-task probabilmente)

--- SocialPulse(noCC)/make_dataset.py
+++ SocialPulse(noCC)/make_dataset.py
@@ -4,33 +4,43 @@ import geopandas as gpd
 
 from pathlib import Path
 
+#data_path = Path('./data/raw')   #Per Cookiecutter
 data_path = Path('./data/raw')
 
-files = {'grid':'trentino-grid.geojson',
-         'adm_reg':'administrative_regions_Trentino.json',
-        'weather':'meteotrentino-weather-station-data.json',
-        'precip':'precipitation-trentino.csv',
-        'precip-avail':'precipitation-trentino-data-availability.csv',
-        'SET-1':'SET-nov-2013.csv',
-        'SET-2':'SET-dec-2013.csv',
-        'SET-lines':'line.csv',
-        'twitter':'social-pulse-trentino.geojson'}
 
-def safe_import(filename, ftype):
+
+files = {'grid':['trentino-grid.geojson',"geojson"],
+        'adm_reg':['administrative_regions_Trentino.json',"json"],
+        'weather':['meteotrentino-weather-station-data.json',"json"],
+        'precip':['precipitation-trentino.csv',"csv"],
+        'precip-avail':['precipitation-trentino-data-availability.csv',"csv"],
+        'SET-1':['SET-nov-2013.csv',"csv"],
+        'SET-2':['SET-dec-2013.csv',"csv"],
+        'SET-lines':['line.csv,"csv"'],
+        'twitter':['social-pulse-trentino.geojson',"json"]}
+
+def safe_import(inp):
     """
     Function that imports data from a file, turns it into a pandas dataframe,
     and prints the types of every variable to check for correctness of import
     """
-    fl=data_path / files[filename]
-    if(ftype=="geojson"):
-        out=gpd.read_file(fl)
+    filename=files[inp][0]
+    filetype=files[inp][1]
 
+    fl=data_path / filename
+    if(filetype=="geojson"):
+        out=gpd.read_file(fl)
+    if(filetype=="csv"):
+        out=pd.read_csv(fl)
+        ##### DA FIXARE
+    if(filetype=="json"):
+        out=pd.read_json(fl, orient="values")
     print(out)
+    
 
     return out
 
 
 
 
-regions=safe_import("grid", "geojson")
 
