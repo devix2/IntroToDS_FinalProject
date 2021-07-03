3f7a77caa7baa4d7c016b0522c2d3892357a3d17
IZerab <beltramelorenzo1999@gmail.com>
2021-07-03 17:24:48 +0200

    Devo saltare fuori casa

--- SocialPulse(noCC)/Test.py
+++ SocialPulse(noCC)/Test.py
@@ -1,46 +1,34 @@
+
+import numpy as np
 import pandas as pd
-from pyecharts.charts import Pie
-from pyecharts import options as opts
-
-# Prepare data
-provinces = [10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4]
-num = [10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4,10,1,3,4]
-#color_series = ['#FAE927','#E9E416','#C9DA36','#9ECB3C']
-            
-# Create data frame
-#df = pd.DataFrame({'provinces': provinces, 'num': num})
-# Descending order
-#df.sort_values(by='num', ascending=False, inplace=True)
-
-# Extract data 
-#v = df['provinces'].values.tolist()
-#d = df['num'].values.tolist()
-
-v=provinces
-d=num
-print(v)
-print(d)
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import json
+from shapely.geometry import Point, Polygon
 
 
-# Instantiate the Pie class
-pie1 = Pie(init_opts=opts.InitOpts(width='1350px', height='750px'))
-# Set color
-#pie1.set_colors(color_series)
-# Add data, set the radius of the pie chart, whether to display it as a Nightingale chart
-pie1.add("", [list(z) for z in zip(v, d)],
-    radius=["30%", "135%"],
-    center=["50%", "65%"],
-    rosetype="area"
-    )
-# Set global configuration items
-pie1.set_global_opts(title_opts=opts.TitleOpts(title='rose chart example'),
-                    legend_opts=opts.LegendOpts(is_show=False),
-                    toolbox_opts=opts.ToolboxOpts())
-# Set series configuration items
-pie1.set_series_opts(label_opts=opts.LabelOpts(is_show=True, position="inside", font_size=12,
-                                                                                            formatter="{b}:{c}day", font_style="italic",
-                                            font_weight="bold", font_family="Microsoft YaHei"
-                                            ),
-                    )
-# Generate html document
-pie1.render('Nightingale rose map.html')
+from functions import *
+import make_dataset as m_d
+
+weather_json = json.load( open(m_d.data_path / m_d.files['weather'][0]) )
+weather = gpd.GeoDataFrame(weather_json['features'])
+
+
+#Elimino le colonne del vento (dati molto incompleti)
+weather.drop(weather.columns[list(range(202,298))], axis=1, inplace=True)
+weather.drop(columns=['minWind', "maxWind"], inplace=True)
+
+#Svolgiamo infine i punti geometrici
+weather['geometry'] = weather['geomPoint.geom'].apply(lambda x:Point(x['coordinates'][0], x['coordinates'][1]))
+weather.drop(columns=['geomPoint.geom'],inplace=True)
+
+pd.set_option('display.max_columns', None)
+weather.head(5)
+
+
+
+
+Tw_final=pd.read_csv("data/processed/twitter_final.csv")
+
+
+a=m_d.df_reg(Tw_final,weather)
