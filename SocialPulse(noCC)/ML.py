7d7bdb512aed0a90c7d4fc88c43a33e4cd75fa59
IZerab <beltramelorenzo1999@gmail.com>
2021-07-05 00:44:06 +0200

    LEGGIMI, READ ME
    Sostanzialmente ho copiato la  parte per la regressione aggiustando i nomi, si dovrebbero fare delle funzioni che contengano ed è quello che farò domani mattina.
    
    IMPORTANTE: ho aggiunto una descrizione in un commento di quelli tra virgolette di come inseriamo i dati, sei d'accordo?

--- SocialPulse(noCC)/ML.py
+++ SocialPulse(noCC)/ML.py
@@ -74,26 +74,26 @@ I dati saranno quindi nella forma
     X = ((Parametri giorno i-2),(parametri giorno i-1))
     y = #tweets giorno i"
 """
-pipe_RF = Pipeline([
+pipe_RFR = Pipeline([
             ('encoder', OneHotEncoder(sparse = False, handle_unknown = 'ignore' )),
             ('scaler', StandardScaler()),
-            ('Regressor', RandomForestRegressor())   #IMPORTATE: dare i parametri alla RFR
+            ('Regressor', RandomForestRegressor())
     ])
-# ale la pena fare un tentativo prima della CV
-pipeRF.fit(X_train, y_train)
+# Vale la pena fare un tentativo prima della CV
+pipeRFR.fit(X_train, y_train)
 
 
 # Provo con una grid search CV
 
 CV_parameters = {'bootstrap': [True, False],
- 'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
- 'max_features': ['auto', 'sqrt'],
- 'min_samples_leaf': [1, 2, 4],
- 'min_samples_split': [2, 5, 10],
- 'n_estimators': [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]
+    'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
+    'max_features': ['auto', 'sqrt'],
+    'min_samples_leaf': [1, 2, 4],
+    'min_samples_split': [2, 5, 10],
+    'n_estimators': [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]
                 }
 # Parametri di Tuning del nostro RFR
-RFR_CV = GridSearchCV(estimator = pipe_RF,
+RFR_CV = GridSearchCV(estimator = pipe_RFR,
                       param_grid = CV_parameters,
                       n_jobs = -1,
                       cv = 3,
@@ -101,4 +101,52 @@ RFR_CV = GridSearchCV(estimator = pipe_RF,
                       )
 best_RF = RFR_CV.fit(X_train, y_train)
 y_RF_pred = best_RF.predict(X_test)
-print(\"Lo score della nostra Random Forest risulta essere: \", accuracy_score(y_test, y_pred_test))"
+print(\"Lo score della nostra Random Forest risulta essere:", accuracy_score(y_test, y_RF_pred))
+
+
+
+##################################################################
+# Classificazione delle circoscrizioni di Trento
+##################################################################
+"""
+Adesso mi occupo di fare la parte di classificazione: voglio individuare quali siano le circoscrizioni di Trento che hanno
+il più alto numero di interazioni sociali basandomi sui dati delle giornate precedenti. 
+Il dataset sarà uguale a quello precedente con la differenza che la parte di conteggi delle X sarà divisa per circoscrizione
+Dal momento che ci sono 12 circoscrizioni i parametri di input saranno 
+[interazioni sociali giorno i-2 divisi per circoscrizione][interazioni sociali giorno i-1 divisi per circoscrizione] + altri parametri
+
+Per quanto riguarda i target, considereremo la circoscrizione con il maggior numero di tweets.
+
+
+Inizialmente provo a lavorare con un Classificatore random forest, dal momento che abbiamo pochi elementi
+"""
+
+pipe_RFC = Pipeline([
+            ('encoder', OneHotEncoder(sparse = False, handle_unknown = 'ignore' )),
+            ('scaler', StandardScaler()),
+            ('Regressor', RandomForestClassifier())
+    ])
+# Vale la pena fare un tentativo prima della CV
+pipeRFC.fit(X_train, y_train)
+
+
+# Provo con una grid search CV
+#Questa griglia va ricontrollata
+CV_parameters = {'bootstrap': [True, False],
+    'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
+    'max_features': ['auto', 'sqrt'],
+    'min_samples_leaf': [1, 2, 4],
+    'min_samples_split': [2, 5, 10],
+    'n_estimators': [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]
+                }
+# Parametri di Tuning del nostro RFR
+RFC_CV = GridSearchCV(estimator = pipe_RFC,
+                      param_grid = CV_parameters,
+                      n_jobs = -1,
+                      cv = 3,
+                      verbose=100
+                      )
+best_RFC = RFC_CV.fit(X_train, y_train)
+y_RFC_pred = best_RFC.predict(X_test)
+print(\"Lo score della nostra Random Forest risulta essere:", accuracy_score(y_test, y_RFC_pred))
+
