1a959f0de75b66216c91e2757839ea0fca2e6d21
IZerab <beltramelorenzo1999@gmail.com>
2021-07-05 19:24:53 +0200

    Push con ML funzionante
    Ho aggiunto le circoscrizioni e pare che stia funzionando, dando risultati pessini, voglio provare con un logistic regressor soft max.
    
    Ho dato una sistemata al codice, ponendo le cose nelle funzioni nel file Supporto_ML

--- SocialPulse(noCC)/ML.py
+++ SocialPulse(noCC)/ML.py
@@ -5,7 +5,6 @@ il tempo in bins di 30 minuti. Questo è dovuto al fatto che, grazie all'EDA, ab
 specialmente nelle ore serali (da qui la necessità di avere una risoluzione temporale abbastanza fine) ma allo stesso 
 tempo avendo 27k records in 62 giorni non volevamo avere dei binning con troppa poca popolazione. Da qui la nostra scelta
 """
-import pandas as pd
 import numpy as np
 import geopandas as gpd
 import pandas as pd
@@ -23,35 +22,8 @@ from sklearn.model_selection import train_test_split
 
 # custom lib
 # import make_dataset as m_d
+import Supporto_ML as supp_ML
 
-# Importo i dati dal database finale che abbiamo fatto queste sono le features di X
-data = pd.read_csv('data/processed/MachineLearningDB.csv')
-
-# Adesso creo il vettore target y
-twitter_data = pd.read_csv('data/processed/twitter_final.csv')
-target = pd.DataFrame({'Counts': twitter_data.groupby(
-        ['month', 'day']).size()}).reset_index()
-target.drop([target.index[0], target.index[1]],inplace = True)
-target = target['Counts'].to_numpy()
-
-#Splitto train e test
-X_train, X_test, y_train, y_test = train_test_split(data,target,test_size=0.4)
-
-############### Parte di ML ###############
-############### Random Forest Regressor ###############
-pipe_logistic = Pipeline([
-    ('encoder', OneHotEncoder(sparse=False, handle_unknown='ignore')),
-    ('scaler', StandardScaler()),
-    ('regressor', LogisticRegression())
-])
-
-pipe_logistic = pipe_logistic.fit(X_train, y_train)
-y_logistic_pred = pipe_logistic.predict(X_test)
-print(y_logistic_pred)
-print("Lo score del nostro modello logistico risulta essere:", r2_score(y_test, y_logistic_pred))
-
-############### Random Forest Regressor ###############
-# Sono dubbioso sull'effettiva efficacio di questa pipeline però un tentativo và sicuramente fatto!
 """
 Task 1: voglio cercare di predire il numero di tweets del giorno i-esimo a partire da alcuni parametri dei giorni i-1 e i-2.
 I parametri scelti sono: numero di tweets, temperatura, precipitazioni e consumo di corrente elettrica
@@ -60,79 +32,33 @@ I dati saranno quindi nella forma
     X = ((Parametri giorno i-2),(parametri giorno i-1))
     y = #tweets giorno i"
 """
-pipe_RFR = Pipeline([
-    ('encoder', OneHotEncoder(sparse=False, handle_unknown='ignore')),
-    ('scaler', StandardScaler()),
-    ('Regressor', RandomForestRegressor(bootstrap=False))
-])
-# Vale la pena fare un tentativo prima della CV che da un'accuracy 0
-pipe_RFR.fit(X_train, y_train)
-y_RF_pred = pipe_RFR.predict(X_test)
-for i in range(len(y_RF_pred)):
-    y_RF_pred[i] = int(y_RF_pred[i])
-print(y_RF_pred - y_test)
-print("Lo score della nostra Random Forest risulta essere:", r2_score(y_test, y_RF_pred))
 
-# Provo con una grid search CV
+# Importo i dati dal database finale che abbiamo fatto queste sono le features di X
+data = pd.read_csv('data/processed/MachineLearningDB.csv')
 
-CV_parameters = {'Regressor__n_estimators': [5, 10, 25, 50],
-                 'Regressor__max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
-                 'Regressor__max_features': ['auto', 'sqrt'],
-                 'Regressor__min_samples_leaf': [1, 2, 4],
-                 'Regressor__min_samples_split': [2, 5, 10],
-                 }
+# Adesso creo il vettore target y
+target_mattina = data['TargetDay']
+target_sera = data['TargetNight']
+data.drop(columns=['TargetDay', 'TargetNight'], inplace=True)
 
-# Parametri di Tuning del nostro RFR
-grid_pipeline = GridSearchCV(estimator=pipe_RFR,
-                             param_grid=CV_parameters,
-                             verbose=10,
-                             n_jobs=-1,)
-grid_pipeline.fit(X_train,y_train)
-y_RF_pred = grid_pipeline.predict(X_test)
-print("Lo score della nostra Random Forest con CV risulta essere:", r2_score(y_test, y_RF_pred))
+# Regressione a livello regionale
+# Logistic Regressor
+#supp_ML.logistic_regressor_fittato(data, target_mattina, 'mattina')
+#supp_ML.logistic_regressor_fittato(data, target_sera, 'sera')
 
-exit()
+# Random Forest Regressor
+#supp_ML.Random_Forest_Regressor_CV(data, target_mattina, 'mattina')
+#supp_ML.Random_Forest_Regressor_CV(data, target_sera, 'sera')
 
-##################################################################
-# Classificazione delle circoscrizioni di Trento
-##################################################################
+
 """
 Adesso mi occupo di fare la parte di classificazione: voglio individuare quali siano le circoscrizioni di Trento che hanno
 il più alto numero di interazioni sociali basandomi sui dati delle giornate precedenti. 
 Il dataset sarà uguale a quello precedente con la differenza che la parte di conteggi delle X sarà divisa per circoscrizione
 Dal momento che ci sono 12 circoscrizioni i parametri di input saranno 
 [interazioni sociali giorno i-2 divisi per circoscrizione][interazioni sociali giorno i-1 divisi per circoscrizione] + altri parametri
-
 Per quanto riguarda i target, considereremo la circoscrizione con il maggior numero di tweets.
-
-
 Inizialmente provo a lavorare con un Classificatore random forest, dal momento che abbiamo pochi elementi
 """
-
-pipe_RFC = Pipeline([
-    ('encoder', OneHotEncoder(sparse=False, handle_unknown='ignore')),
-    ('scaler', StandardScaler()),
-    ('Regressor', RandomForestClassifier())
-])
-# Vale la pena fare un tentativo prima della CV
-pipe_RFC.fit(X_train, y_train)
-
-# Provo con una grid search CV
-# Questa griglia va ricontrollata
-CV_parameters = {'bootstrap': [True, False],
-                 'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],
-                 'max_features': ['auto', 'sqrt'],
-                 'min_samples_leaf': [1, 2, 4],
-                 'min_samples_split': [2, 5, 10],
-                 'n_estimators': [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000]
-                 }
-# Parametri di Tuning del nostro RFR
-RFC_CV = GridSearchCV(estimator=pipe_RFC,
-                      param_grid=CV_parameters,
-                      n_jobs=-1,
-                      cv=3,
-                      verbose=100
-                      )
-best_RFC = RFC_CV.fit(X_train, y_train)
-y_RFC_pred = best_RFC.predict(X_test)
-print("Lo score della nostra Random Forest risulta essere:", accuracy_score(y_test, y_RFC_pred))
+# Random Forest Classifier
+supp_ML.Random_Forest_Classifier_Circoscrizione(data)
---
+++ SocialPulse(noCC)/Supporto_ML.py
@@ -0,0 +1,125 @@
+# Libreria per ML
+import numpy as np
+import geopandas as gpd
+import pandas as pd
+
+# funzioni di sk-learn
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV, cross_validate
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, RobustScaler
+from sklearn.metrics import matthews_corrcoef, r2_score, accuracy_score, confusion_matrix, plot_confusion_matrix
+from sklearn.model_selection import train_test_split
+
+# custom lib
+# import make_dataset as m_d
+
+
+
+
+
+
+############### Random Forest Regressor ###############
+def logistic_regressor_fittato(X,y, time):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+    pipe_logistic = Pipeline([
+        ('encoder', OneHotEncoder(sparse=False, handle_unknown='ignore')),
+        ('scaler', StandardScaler()),
+        ('regressor', LogisticRegression())
+    ])
+
+    pipe_logistic = pipe_logistic.fit(X_train, y_train)
+    y_logistic_pred = pipe_logistic.predict(X_test)
+    print("Lo score del nostro modello logistico risulta essere:", r2_score(y_test, y_logistic_pred),'per la ', time)
+
+
+
+
+
+def Random_Forest_Regressor_CV(X,y, time):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4)
+    pipe_RFR = Pipeline([
+        ('encoder', OneHotEncoder(sparse=False, handle_unknown='ignore')),
+        ('scaler', StandardScaler()),
+        ('Regressor', RandomForestRegressor(bootstrap=False))
+    ])
+    # Vale la pena fare un tentativo prima della CV che da un'accuracy 0
+    pipe_RFR.fit(X_train, y_train)
+    y_RF_pred = pipe_RFR.predict(X_test)
+    for i in range(len(y_RF_pred)):
+        y_RF_pred[i] = int(y_RF_pred[i])
+    print("Lo score della nostra Random Forest risulta essere:", r2_score(y_test, y_RF_pred), 'alla ', time)
+
+    # Provo con una grid search CV
+
+    CV_parameters = {'Regressor__n_estimators': [5, 10, 25, 50],
+                     'Regressor__max_depth': [10, 20, 50, 70, 100, None],
+                     'Regressor__max_features': ['auto', 'sqrt'],
+                     'Regressor__min_samples_leaf': [1, 2, 4],
+                     'Regressor__min_samples_split': [2, 5, 10],
+                     }
+
+    # Parametri di Tuning del nostro RFR
+    grid_pipeline = GridSearchCV(estimator=pipe_RFR,
+                                 param_grid=CV_parameters,
+                                 verbose=1,
+                                 cv=3,
+                                 n_jobs=-1, )
+    grid_pipeline.fit(X_train, y_train)
+    y_RF_pred = grid_pipeline.predict(X_test)
+    print("Lo score della nostra Random Forest con CV risulta essere:", r2_score(y_test, y_RF_pred), 'alla ', time)
+
+
+######## CLASSIFICAZIONE CIRCOSCRIZIONI ##############
+def circoscrizione_attiva(link):
+    tw=pd.read_csv(link)
+
+    tw.dropna(subset=["circoscrizione"], inplace=True)
+    tw.reset_index(inplace=True)
+
+    temp=tw.groupby(["month", "day","circoscrizione"]).size()
+    maxi=temp.groupby(["month", "day"]).idxmax()
+
+    output=[i[2] for i in maxi]
+    print(output)
+    return output
+
+
+
+def Random_Forest_Classifier_Circoscrizione(data):
+    # creo il vettore delle y trovando qual è la circoscrizione più attiva
+    target = circoscrizione_attiva("data/processed/twitter_final.csv")
+    target = pd.Series(target)
+    target.drop([target.index[0], target.index[1]], inplace=True)
+    enc = OneHotEncoder(sparse=False, handle_unknown='ignore')
+    target = enc.fit_transform(target.values[:,None])
+
+    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.4)
+
+    pipe_RFC = Pipeline([
+        ('encoder', OneHotEncoder(sparse=False, handle_unknown='ignore')),
+        ('scaler', StandardScaler()),
+        ('Regressor', RandomForestClassifier(bootstrap=False))
+    ])
+
+    # Provo con una grid search CV
+    # Questa griglia va ricontrollata
+    CV_parameters = {'Regressor__n_estimators': [5, 10, 25, 50],
+                     'Regressor__max_depth': [10, 20, 50, 70, 100, None],
+                     'Regressor__max_features': ['auto', 'sqrt'],
+                     'Regressor__min_samples_leaf': [1, 2, 4],
+                     'Regressor__min_samples_split': [2, 5, 10],
+                     }
+    # Parametri di Tuning del nostro RFR
+    RFC_CV = GridSearchCV(estimator=pipe_RFC,
+                          param_grid=CV_parameters,
+                          n_jobs=-1,
+                          cv=3,
+                          verbose=1
+                          )
+    RFC_CV.fit(X_train, y_train)
+    y_RFC_pred = RFC_CV.predict(X_test)
+    print("Lo score della nostra Random Forest risulta essere:", r2_score(y_test, y_RFC_pred), 'per il riconoscimento delle circoscrizioni più attive')
